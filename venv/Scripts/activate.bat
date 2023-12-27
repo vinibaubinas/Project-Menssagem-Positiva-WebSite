42710c47db4da537ff719e8886bdea4798ed5ade
vinibaubinas <vinibaubinas@gmail.com>
2023-12-27 14:44:40 -0300

    Project Mensagem Positiva

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\W11\Desktop\Projeto de detector de moviemtos\venv
+set VIRTUAL_ENV=C:\Users\W11\Desktop\Projeto de Mensagens Positivas\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
